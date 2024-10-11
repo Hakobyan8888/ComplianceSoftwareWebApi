3231c27ea153a58b6398cee70d4d90d2647df652
Hakobyan8888 <ha.stepan02@gmail.com>
2024-10-11 00:01:36 -0600

    changed sector code to string and added new project for reading excel info and putting to db

---
+++ ComplianceSoftwareWebApi/Migrations/20241011015522_IndustryLicensesTableSectorCode.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComplianceSoftwareWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class IndustryLicensesTableSectorCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SectorCode",
+                table: "Industries",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SectorCode",
+                table: "Industries",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ ExcelSqlHelper/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ExcelSqlHelper.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelSqlHelper
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Industry> Industries { get; set; }
+        public DbSet<License> Licenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=ComplianceDb;Trusted_Connection=True;MultipleActiveResultSets=true;User Id=Compliance;Password=Compliance;Encrypt=True;TrustServerCertificate=True;");
+        }
+    }
+}
